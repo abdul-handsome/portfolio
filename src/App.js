80c7ec6a579c56e9502a2c5b9a6257e25c1c7428
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-19 00:40:10 +0100

    version 1806_05

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
   return (
     <div>
     <h3 className = 'homePage'>Home</h3>
-    <div className = 'projectContainer'>
+    <div className = 'projectContainer1'>
+    <div className = 'individualProject1_1'>me</div>
+    <div className = 'individualProject1_2'>skills</div>
+    <div className = 'individualProject1_3'>portfolio</div>
+    </div>
+    <div className = 'projectContainer2'>
     <div className = 'projectLabel'>My Projects :</div>
-    <div className = 'individualProject'>Soccer Pot</div>
-    <div className = 'individualProject'>Kodflix</div>
-    <div className = 'individualProject'>yyyy yyyy yyyy</div>
+    <div className = 'individualProject1'>Soccer Pot</div>
+    <div className = 'individualProject2'>Kodflix</div>
+    <div className = 'individualProject3'>Platform</div>
     </div>
     </div>
 
