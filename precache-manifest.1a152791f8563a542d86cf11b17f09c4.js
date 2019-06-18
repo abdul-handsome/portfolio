10fa065952ec995a7181a3ba1a79c51fbb3c46a2
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-18 11:43:55 +0100

    Updates

---
+++ precache-manifest.1a152791f8563a542d86cf11b17f09c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2cbbba76c5ab458711033aea40d88b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b02f2ad106e7c5648617",
+    "url": "/portfolio/static/css/main.866b4e6a.chunk.css"
+  },
+  {
+    "revision": "3901b905dd3ab1d0cf9b",
+    "url": "/portfolio/static/js/2.7dceac83.chunk.js"
+  },
+  {
+    "revision": "b02f2ad106e7c5648617",
+    "url": "/portfolio/static/js/main.b9206408.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
