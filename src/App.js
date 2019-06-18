5f97ba32255ce020895d50c18923a34c780ae54a
Abdul Bello <crownkunle@sikibell.onmicrosoft.com>
2019-06-18 12:00:49 +0100

    small changes to test deployment.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello !
+          hello ! small changes
         </a>
       </header>
     </div>
